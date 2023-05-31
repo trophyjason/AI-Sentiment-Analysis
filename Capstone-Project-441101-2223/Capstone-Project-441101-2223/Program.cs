98e83fbf093ff497936a8fedcb45cab37ec0163a
trophyjason <134587709+trophyjason@users.noreply.github.com>
2023-05-31 23:35:45 +0100

    Done

--- Capstone-Project-441101-2223/Capstone-Project-441101-2223/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Capstone_Project_441101_2223;
-
-Console.WriteLine("Hello, Capstone!");
-
-ProjectManager manager = new ProjectManager();
-
-new ProjectManagerMenu(manager).Select();
